58e81c5b99a9b9858e8bf890524bfbf4406eae9c
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-20 22:41:29 -0500

    finished all methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,20 +3,36 @@ module.exports = {
   fail,
   repair,
   get,
-};
+}
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 }
+  } else if (item.enhancement === 15) {
+    return { ...item, durability: item.durability - 10 }
+  } else if (item.enhancement >= 16) {
+    return {
+      ...item,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1,
+    }
+  }
+  return { ...item }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 }
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement > 0) {
+    return { ...item, name: `[+${item.enhancement}]${item.name}` }
+  }
+  return { ...item }
 }
